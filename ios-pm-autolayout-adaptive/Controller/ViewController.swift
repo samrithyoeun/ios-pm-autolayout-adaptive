1d02fe2a26de22402a4c653a32b2a2e4f7b1f009
samrith <samrith.yoeun@gmail.com>
2018-06-25 11:15:38 +0700

    first commit

---
+++ ios-pm-autolayout-adaptive/Controller/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  ios-pm-autolayout-adaptive
+//
+//  Created by Samrith Yoeun on 6/25/18.
+//  Copyright © 2018 samrith. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController  {
+
+
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       print(UIDevice.modelName)
+        if UIDevice.modelName.contains("Plus") || UIDevice.modelName.contains("X") {
+            
+            blueView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+            blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:0).isActive = true
+            
+            redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+            redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            
+            
+            greenView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+            greenView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+            
+
+        }else{
+            
+            greenView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            greenView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+            
+            redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+            redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            
+            blueView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+            blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:0).isActive = true
+            
+        }
+    }
+    
+    
+}
+
+
+
+
+
